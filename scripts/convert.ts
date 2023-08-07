558e6786bccd89c015e5047cb1caa3426f261e55
Dmitry Fisenko <fisenkodv@gmail.com>
2023-08-07 00:27:16 -0400

    feat: add types

--- scripts/convert.ts
+++ scripts/convert.ts
@@ -7,6 +7,36 @@ const DEFAULT_COLOR_SCHEMES_MANAGER_URL = "https://raw.githubusercontent.com/Jet
 
 // https://github.com/JetBrains/colorSchemeTool
 
+interface Option {
+    name: string;
+    value: OptionValue[];
+}
+
+interface OptionValue {
+    name: OptionValueName;
+    value?: string;
+    deuteranopia?: string;
+    protanopia?: string;
+}
+
+enum OptionValueName {
+    FOREGROUND,
+    BACKGROUND,
+    FONT_TYPE,
+    EFFECT_COLOR,
+    EFFECT_TYPE
+}
+
+interface Scheme {
+    name: string;
+    colors: OptionValue[];
+    attributes: Option[];
+}
+
+function loadScheme(xml: string, name: string): Scheme {}
+
+function map(scheme: Scheme) {}
+
 async function main() {
     const response = await axios.get(DEFAULT_COLOR_SCHEMES_MANAGER_URL);
     const xml = response.data;
